8a483677c95d4e0d707080590f2078ce2ba71683
Vini <viniciusdemourarosa@gmail.com>
2025-03-23 21:36:59 -0300

    feat: add nav header to layout from root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
+import { ThemeProvider } from "@/components/theme-provider"
 
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import NavHeader from "@/components/nav-header"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,14 +21,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-      <SidebarProvider>
-        <AppSidebar />
-        <main>
-          <SidebarTrigger />
-          {children}
-        </main>
-        </SidebarProvider>
+      <body className={inter.className} suppressHydrationWarning>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <SidebarProvider>
+          <div className="w-full flex flex-col md:flex-row justify-between">
+            
+            <div className="hidden md:block">
+              <AppSidebar />
+            </div>
+
+            <main className="antialiased flex-1 h-screen bg-muted overflow-y-auto">
+              <div className="flex justify-between py-4 px-3">
+                <div className="block md:hidden">
+                  <SidebarTrigger />
+                </div>
+                <NavHeader />
+              </div>
+              {children}
+            </main>
+          
+          </div>
+          </SidebarProvider>
+      </ThemeProvider>
       </body>
     </html>
   )
