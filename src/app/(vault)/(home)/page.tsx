50e2a7967981fbd7864bd2b95e11c41c3ec8d098
Vini <viniciusdemourarosa@gmail.com>
2025-03-23 21:37:28 -0300

    feat: add workflow page

--- src/app/(vault)/(home)/page.tsx
+++ src/app/(vault)/(home)/page.tsx
@@ -1,5 +1,19 @@
-export default function Home() {
+import { ChartAreaInteractive } from "@/components/chart-area-interactive"
+import { DataTable } from "@/components/data-table"
+
+import data from "./data.json"
+
+export default function Page() {
   return (
-    <h1>Hello World</h1>
+    <div className="w-full flex justify-center flex-1 flex-col">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="w-80vw flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          <div className="px-4 lg:px-6">
+            <ChartAreaInteractive />
+          </div>
+          <DataTable data={data} />
+        </div>
+      </div>
+    </div>
   )
 }
